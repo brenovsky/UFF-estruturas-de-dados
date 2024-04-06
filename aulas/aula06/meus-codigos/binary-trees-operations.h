b36c17e60b45e9e72c26128430bb928634bca311
Breno de Carvalho Correia <brenodecarvalhocorreia@gmail.com>
2024-04-06 20:21:44 -0300

    add: first commit of binary trees

---
+++ aulas/aula06/meus-codigos/binary-trees-operations.h
@@ -0,0 +1,17 @@
+#ifndef BINARY_TREES_OPERATIONS_H
+#define BINARY_TREES_OPERATIONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "listas-encadeadas-funcoes.h"
+
+typedef struct bitree {
+    int info;
+    struct TLista* esq;
+    struct TLista* dir;
+} TTree;
+
+
+
+
+#endif //BINARY_TREES_OPERATIONS_H
