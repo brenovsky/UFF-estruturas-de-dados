9e69fa7715693e9d2011824ddb7da5debf2c42ea
Breno de Carvalho Correia <brenodecarvalhocorreia@gmail.com>
2024-04-05 23:55:48 -0300

    add: exercises from class

---
+++ aulas/aula05/exercicios/pilha-exercicio.c
@@ -0,0 +1,80 @@
+#include "pilha-lista.h"
+
+TPilha *inicializa() {
+    TPilha *pilha = (TPilha *)malloc(sizeof(TPilha));
+    pilha->topo = NULL;
+    return pilha;
+}
+
+void libera(TPilha *p) {
+    TLista *q = p->topo;
+    TLista *r;
+    while(q != NULL){
+        r = q;
+        q=q->prox;
+        free(r);
+    }
+    free(p);
+}
+
+int pilha_vazia(TPilha *pilha) {
+      if (pilha->topo == NULL)
+      	return 1; //pilha vazia
+      else
+      	return 0; //pilha tem pelo menos 1 elemento
+}
+
+/* *
+ * Insere elem no topo da pilha
+ * */
+void push(TPilha *pilha, int elem) {
+    TLista *novo = (TLista*) malloc(sizeof(TLista));
+    novo->info = elem;
+    novo->prox = pilha->topo;
+    pilha->topo = novo;
+}
+
+/* *
+ * Altera o elemento do topo da pilha
+ * */
+void altera_topo(TPilha *pilha, int elem) {
+	//TODO
+}
+
+/* *
+ * Exclui o elemento do topo da pilha
+ * retorna o info do elemento excluído
+ */
+int pop(TPilha *pilha) {
+	//TODO
+}
+
+/* *
+ * Consulta o elemento do topo da pilha
+ * retorna info do elemento do topo
+ */
+int peek(TPilha *pilha) {
+    	//TODO
+}
+
+void imprime_pilha(TPilha *pilha) {
+    //TODO
+}
+
+int main() {
+    TPilha *pilha;
+    pilha = inicializa();
+    push(pilha, 5);
+    printf("topo: %d\n", peek(pilha));
+    push(pilha, 4);
+    printf("topo: %d\n", peek(pilha));
+    push(pilha, 3);
+    printf("topo: %d\n", peek(pilha));
+
+    imprime_pilha(pilha);
+
+    int info = pop(pilha);
+    printf("elemento removido: %d\n", info);
+
+    imprime_pilha(pilha);
+}
